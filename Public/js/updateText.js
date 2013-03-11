76b09da6ddf3cd0952c74ac25d2cb1adca190dc7
zjc <zjc@jcnip.com>
2013-03-11 10:44:27 +0800

    create picture

--- Public/js/updateText.js
+++ Public/js/updateText.js
@@ -144,6 +144,9 @@
  	var answer=answerSingle?answerSingle:multiAnswer;
  	var point=document.getElementById('point').value;
  	var question=editor.html();
+  question = question.replace(/%/g, "%25");  
+  question= question.replace(/\&/g, "%26");  
+  question= question.replace(/\+/g, "%2B");
   var level=$("#difficult").val();
   var tips=answer==""?"答案不能为空":"";
   tips+=tips==""?aspects=="[]"?"知识点不能为空":"":aspects=="[]"?",知识点不能为空":"";
@@ -159,9 +162,25 @@
        dataType:"text",
        success:function(d){
           if(test_type!=4){
-            testInfo();
+            $.ajax({
+              url:"/Admin/Picture/update_html",
+              type:"post",
+              data:"testId="+id,
+              dataType:"text",
+              success:function(ed){
+                testInfo();
+              }
+            })
           }else{
-            setsTitleInfo();
+            $.ajax({
+              url:"/Admin/Picture/update_html",
+              type:"post",
+              data:"testId="+id,
+              dataType:"text",
+              success:function(fd){
+                setsTitleInfo();
+              }
+            })      
           }
        }    
      })
