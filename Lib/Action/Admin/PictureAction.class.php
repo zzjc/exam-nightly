077f8838d14aa92846888ece14f76ea3d6f99cb9
panxianhai <panxianhai@gmail.com>
2013-03-11 09:40:36 +0800

    调整phantomjs

--- Lib/Action/Admin/PictureAction.class.php
+++ Lib/Action/Admin/PictureAction.class.php
@@ -1,29 +1,29 @@
 <?php
-	require 'Common/Json.php';
-	import('ORG.Util.Input');
-	class PictureAction extends Action{
-	    public function update_html(){
-	        header("content-type:text/html;charset=utf8");
-	        $testId=Input::getVar($_POST["testId"]);
-	        //重新生成html
-	        $dir = 'Data/html';
-	        $template = "Data/template.html"; 
-	        $template_html = file_get_contents($template);
-	        $exam_test = M('test');
-	        $test=$exam_test->where("id=".$testId)->field('content')->find(); 
-	        if($test) {
-	            unlink('Data/html/' .$testId.'.html');
-	            //替换模板内容，生成新模板，往数据库插入图片地址
-	            $new = str_replace('{REPLACE_HOLDER}', $test['content'], $template_html);
-	            $html_name = $dir . '/' . $testId. '.html';
-	            file_put_contents($html_name, $new);
-	            //生成图片
-	            $url="cd /home/test\n\r../command/phantomjs/bin/phantomjs rasterize.js "."Data/html/".$testId.".html Storage/image480/".$testId.".png";
-	            file_put_contents('Com.sh',$url);
-	            exec("Com.sh"); 
-	        }else{
-	            echo '修改失败';  
-	        }	
+require 'Common/Json.php';
+import('ORG.Util.Input');
+class PictureAction extends Action{
+    public function update_html(){
+        header("content-type:text/html;charset=utf8");
+        $testId=Input::getVar($_POST["testId"]);
+        //重新生成html
+        $dir = 'Data/html';
+        $template = "Data/template.html";
+        $template_html = file_get_contents($template);
+        $exam_test = M('test');
+        $test=$exam_test->where("id=".$testId)->field('content')->find();
+        if($test) {
+            unlink('Data/html/' .$testId.'.html');
+            //替换模板内容，生成新模板，往数据库插入图片地址
+            $new = str_replace('{REPLACE_HOLDER}', $test['content'], $template_html);
+            $html_name = $dir . '/' . $testId. '.html';
+            file_put_contents($html_name, $new);
+            //生成图片
+            $document_root = C('DOCUMENT_ROOT');
+            $command = "cd $document_root;phantomjs rasterize.js Data/html/{$testId}.html Storage/image480/{$testId}.png";
+            exec($command);
+        }else{
+            echo '修改失败';
+        }
 
-	    }
-	}    
+    }
+}
