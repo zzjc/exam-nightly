c6dfb0ea1208dc525da57a35ebca6fbea7cb60c6
zjc <zjc@jcnip.com>
2013-04-18 14:06:10 +0800

    level point level

--- Public/js/addTest.js
+++ Public/js/addTest.js
@@ -80,43 +80,43 @@
       $("#titleSets").bind("click",selectSetsType);
       $("#titleSingle").bind("click",openTitleSingleDel);
     })  
-  var editor;
-  KindEditor.ready(function(K) {
-          editor=K.create('#editor_id');
-  });
- //打开材料分析题的子题
- function selectSetsType(){
-    art.dialog({
-               id:"setsType",
-               title:'选择题目类型',
-               follow: document.getElementById('titleSets'),
-               content: "<input type='radio' value='1' name='setsType[]'/>单选题<br /><br />" +
-                       "<input type='radio' value='2' name='setsType[]' />多选题<br /><br />"+
-                       "<input type='radio' value='3' name='setsType[]'>判断题",
-               ok: function () {             
-                   var setsType=$("input[name='setsType[]']:checked").val();
-                   switch(setsType){
-                      case "1":
-                        art.dialog.data("setsType",setsType);
-                        art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:460,title:'添加单选题'},false);
-                      break;
-                      case "2":
-                        art.dialog.data("setsType",setsType);
-                        art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:460,title:'添加多选题'},false);
+    var editor;
+    KindEditor.ready(function(K) {
+            editor=K.create('#editor_id');
+    });
+   //打开材料分析题的子题
+   function selectSetsType(){
+      art.dialog({
+                 id:"setsType",
+                 title:'选择题目类型',
+                 follow: document.getElementById('titleSets'),
+                 content: "<input type='radio' value='1' name='setsType[]'/>单选题<br /><br />" +
+                         "<input type='radio' value='2' name='setsType[]' />多选题<br /><br />"+
+                         "<input type='radio' value='3' name='setsType[]'>判断题",
+                 ok: function () {             
+                     var setsType=$("input[name='setsType[]']:checked").val();
+                     switch(setsType){
+                        case "1":
+                          art.dialog.data("setsType",setsType);
+                          art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:460,title:'添加单选题'},false);
                         break;
-                      case "3":
-                        art.dialog.data("setsType",setsType);
-                        art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:460,title:'添加判断题'},false);
-                        break;
-                   }
-               }
-           });
+                        case "2":
+                          art.dialog.data("setsType",setsType);
+                          art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:460,title:'添加多选题'},false);
+                          break;
+                        case "3":
+                          art.dialog.data("setsType",setsType);
+                          art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:460,title:'添加判断题'},false);
+                          break;
+                     }
+                 }
+             });
+    }
+    //打开材料分析题
+   function openTitleSetsDel(){
+    art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:480,title:'添加题目'}, false);
   }
-  //打开材料分析题
- function openTitleSetsDel(){
-  art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:480,title:'添加题目'}, false);
-}
-//打开单选；多选；判断
-function openTitleSingleDel(){
-  art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:480,title:'添加题目'},false);
-}
+  //打开单选；多选；判断
+  function openTitleSingleDel(){
+    art.dialog.open("/Tpl/default/Admin/Test/artdialogTitle.html",{fixed: true,id:'memdiv',width:500,height:480,title:'添加题目'},false);
+  }
